33dce716a93ce0ce7c338d06ce28b15968466f2a
Vlad Tomici <vladtomici@gmail.com>
2024-03-27 00:44:21 +0200

    Added a first working version of the code

--- DocsScan/ContentView.swift
+++ DocsScan/ContentView.swift
@@ -8,14 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showScannerSheet = false;
+    @State private var texts:[ScanData] = []
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                if texts.count > 0 {
+                    List {
+                        ForEach(texts) { text in NavigationLink(
+                            destination: ScrollView {
+                                Text(text.content)
+                            },
+                            label: {
+                                Text(text.content).lineLimit(1)
+                            }
+                        )
+                        }
+                    }
+                } else {
+                    Text("No scan done yet!").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                }
+            }
+            .navigationTitle("DOCScan")
+            .navigationBarItems(
+                trailing:
+                    Button(
+                        action: {
+                            self.showScannerSheet = true;
+                        },
+                        label: {
+                            Image(systemName: "doc.text.viewfinder")
+                                .font(.title)
+                        }
+                    )
+                    .sheet(isPresented: $showScannerSheet, content: {
+                        makeScannerView()
+                    })
+            )
         }
-        .padding()
+    }
+    
+    private func makeScannerView() -> ScannerView {
+        ScannerView(completionHandler: {
+            textPerPage in
+            if let outputText = textPerPage?
+                .joined(separator: "\n")
+                .trimmingCharacters(in: .whitespacesAndNewlines) {
+                let newScanData = ScanData(content: outputText)
+                self.texts.append(newScanData)
+            }
+            self.showScannerSheet = false
+        })
     }
 }
 
