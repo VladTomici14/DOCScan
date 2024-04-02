37ee8214af1655c1698cf8a57bd9cc86a6399617
Vlad Tomici <vladtomici@gmail.com>
2024-04-02 11:33:02 +0300

    Created an object for each input form that will be used through the app

---
+++ DocsScan/Components/SettingsRowView.swift
@@ -0,0 +1,33 @@
+//
+//  SettingsRowView.swift
+//  DocsScan
+//
+//  Created by Vlad Tomici on 01.04.2024.
+//
+
+import SwiftUI
+
+struct SettingsRowView: View {
+    
+    let imageName: String
+    let title: String
+    let tintColor: Color
+    
+    var body: some View {
+        HStack(spacing: 12) {
+            Image(systemName: imageName)
+                .imageScale(.small)
+                .font(.title)
+                .foregroundColor(tintColor)
+            
+            Text(title)
+                .font(.subheadline)
+                .foregroundColor(.white)
+        }
+        
+    }
+}
+
+#Preview {
+    SettingsRowView(imageName: "gear", title: "settings", tintColor: Color(.systemGray))
+}
