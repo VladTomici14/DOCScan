78f3e24e5e2460a5a75d3f8cb7114a121883a539
Vlad Tomici <vladtomici@gmail.com>
2024-04-02 11:30:53 +0300

    Created an object for each input form that will be used through login

---
+++ DocsScan/Components/InputView.swift
@@ -0,0 +1,44 @@
+//
+//  InputView.swift
+//  DocsScan
+//
+//  Created by Vlad Tomici on 29.03.2024.
+//
+
+import SwiftUI
+
+struct InputView: View {
+    
+    @Binding var text: String
+    let title: String
+    let placeHolder: String
+    var isSeculreField = false
+    
+    var body: some View {
+
+        VStack (alignment: .leading, spacing: 12) {
+            Text(title)
+                .foregroundColor(Color(.white))
+                .fontWeight(.semibold)
+                .font(.footnote)
+            
+            if isSeculreField {
+                SecureField(placeHolder, text: $text).font(.system(size: 14))
+            } else {
+                TextField(placeHolder, text: $text).font(.system(size: 14))
+            }
+        }
+        
+    }
+}
+
+
+struct InputView_Previews: PreviewProvider {
+    static var previews: some View {
+        InputView(text: .constant(""), title: "Email Address", placeHolder: "name@example.com")
+    }
+}
+
+//#Preview {
+//    InputView()
+//}
