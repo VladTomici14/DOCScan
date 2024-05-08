c4a5c7cabc687e628894fbb2fc83824bfe65212e
Vlad Tomici <vladtomici@gmail.com>
2024-05-08 14:56:37 +0300

    Added init for firebase

--- DocsScan/App/DocsScanApp.swift
+++ DocsScan/App/DocsScanApp.swift
@@ -8,21 +8,22 @@
 import SwiftUI
 import Firebase
 
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
 @main
 struct DocsScanApp: App {
-    
-    @StateObject var viewModel = AuthViewModel()
-    
-    init() {
-        FirebaseApp.configure()
-    }
+        
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
     var body: some Scene {
         WindowGroup {
-//            ContentView2()
-//                .environmentObject(viewModel)
-//                .preferredColorScheme(.dark)
-            
             ContentView()
         }
     }
